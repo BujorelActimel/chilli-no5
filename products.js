190b17859299905d0862a7355aca5548d2bb1622
bujor <bujormihaialexandru@gmail.com>
2024-08-14 11:19:48 +0300

    added heat levels, added pairings, fixed ios bottom navbar ui

--- products.js
+++ products.js
@@ -1,22 +1,163 @@
+// products.js
 export const products = [
-    { id: '1', name: 'MEXICAN FURY', price: 9.99, image: require('./assets/product-placeholder.png') },
-    { id: '2', name: 'FIERY GAZPACHO', price: 8.99, image: require('./assets/product-placeholder.png') },
-    { id: '3', name: 'HEAVENLY HARISSA', price: 10.99, image: require('./assets/product-placeholder.png') },
-    { id: '4', name: 'JAMAICAN JERK', price: 9.49, image: require('./assets/product-placeholder.png') },
-    { id: '5', name: 'REGAL RED PEPPER', price: 7.99, image: require('./assets/product-placeholder.png') },
-    { id: '6', name: 'TOTALLY THAI', price: 8.49, image: require('./assets/product-placeholder.png') },
-    { id: '7', name: 'SCORCHING SRIRACHA', price: 11.99, image: require('./assets/product-placeholder.png') },
-    { id: '8', name: 'GHOST PEPPER BLAST', price: 14.99, image: require('./assets/product-placeholder.png') },
-    { id: '9', name: 'CAJUN KICK', price: 9.99, image: require('./assets/product-placeholder.png') },
-    { id: '10', name: 'HABANERO HEAT', price: 12.99, image: require('./assets/product-placeholder.png') },
-    { id: '11', name: 'CHIPOTLE CHARM', price: 10.49, image: require('./assets/product-placeholder.png') },
-    { id: '12', name: 'MANGO TANGO', price: 8.99, image: require('./assets/product-placeholder.png') },
-    { id: '13', name: 'GARLIC INFERNO', price: 9.99, image: require('./assets/product-placeholder.png') },
-    { id: '14', name: 'WASABI WILDFIRE', price: 13.99, image: require('./assets/product-placeholder.png') },
-    { id: '15', name: 'CAROLINA REAPER RAGE', price: 15.99, image: require('./assets/product-placeholder.png') },
-    { id: '16', name: 'JALAPENO JUBILEE', price: 7.99, image: require('./assets/product-placeholder.png') },
-    { id: '17', name: 'SCOTCH BONNET SIZZLE', price: 11.49, image: require('./assets/product-placeholder.png') },
-    { id: '18', name: 'PERI-PERI PUNCH', price: 10.99, image: require('./assets/product-placeholder.png') },
-    { id: '19', name: 'TABASCO TORNADO', price: 8.99, image: require('./assets/product-placeholder.png') },
-    { id: '20', name: 'GOCHUJANG GUSTO', price: 12.49, image: require('./assets/product-placeholder.png') },
+    { 
+        id: '1', 
+        name: 'MEXICAN FURY', 
+        price: 9.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 4,
+        pairings: ['Tacos', 'Burritos', 'Quesadillas']
+    },
+    { 
+        id: '2', 
+        name: 'FIERY GAZPACHO', 
+        price: 8.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 3,
+        pairings: ['Grilled Cheese', 'Tomato Soup', 'Bruschetta']
+    },
+    { 
+        id: '3', 
+        name: 'HEAVENLY HARISSA', 
+        price: 10.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 4,
+        pairings: ['Couscous', 'Roasted Vegetables', 'Lamb']
+    },
+    { 
+        id: '4', 
+        name: 'JAMAICAN JERK', 
+        price: 9.49, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 4,
+        pairings: ['Grilled Chicken', 'Rice and Peas', 'Plantains']
+    },
+    { 
+        id: '5', 
+        name: 'REGAL RED PEPPER', 
+        price: 7.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 2,
+        pairings: ['Pasta', 'Sandwiches', 'Eggs']
+    },
+    { 
+        id: '6', 
+        name: 'TOTALLY THAI', 
+        price: 8.49, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 3,
+        pairings: ['Pad Thai', 'Spring Rolls', 'Stir Fry']
+    },
+    { 
+        id: '7', 
+        name: 'SCORCHING SRIRACHA', 
+        price: 11.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 4,
+        pairings: ['Pho', 'Banh Mi', 'Fried Rice']
+    },
+    { 
+        id: '8', 
+        name: 'GHOST PEPPER BLAST', 
+        price: 14.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 5,
+        pairings: ['Chili', 'Spicy Wings', 'Curry']
+    },
+    { 
+        id: '9', 
+        name: 'CAJUN KICK', 
+        price: 9.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 3,
+        pairings: ['Jambalaya', 'Gumbo', 'Blackened Fish']
+    },
+    { 
+        id: '10', 
+        name: 'HABANERO HEAT', 
+        price: 12.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 5,
+        pairings: ['Tacos al Pastor', 'Ceviche', 'Mango Salsa']
+    },
+    { 
+        id: '11', 
+        name: 'CHIPOTLE CHARM', 
+        price: 10.49, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 3,
+        pairings: ['Burritos', 'Grilled Corn', 'Adobo Chicken']
+    },
+    { 
+        id: '12', 
+        name: 'MANGO TANGO', 
+        price: 8.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 2,
+        pairings: ['Grilled Shrimp', 'Fish Tacos', 'Fruit Salad']
+    },
+    { 
+        id: '13', 
+        name: 'GARLIC INFERNO', 
+        price: 9.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 4,
+        pairings: ['Pizza', 'Pasta', 'Garlic Bread']
+    },
+    { 
+        id: '14', 
+        name: 'WASABI WILDFIRE', 
+        price: 13.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 4,
+        pairings: ['Sushi', 'Sashimi', 'Poke Bowls']
+    },
+    { 
+        id: '15', 
+        name: 'CAROLINA REAPER RAGE', 
+        price: 15.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 5,
+        pairings: ['Extreme Chili', 'Spicy Challenges', 'Hot Wings']
+    },
+    { 
+        id: '16', 
+        name: 'JALAPENO JUBILEE', 
+        price: 7.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 2,
+        pairings: ['Nachos', 'Poppers', 'Cornbread']
+    },
+    { 
+        id: '17', 
+        name: 'SCOTCH BONNET SIZZLE', 
+        price: 11.49, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 5,
+        pairings: ['Jerk Chicken', 'Curry Goat', 'Escovitch Fish']
+    },
+    { 
+        id: '18', 
+        name: 'PERI-PERI PUNCH', 
+        price: 10.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 3,
+        pairings: ['Grilled Chicken', 'Roasted Vegetables', 'Spicy Rice']
+    },
+    { 
+        id: '19', 
+        name: 'TABASCO TORNADO', 
+        price: 8.99, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 3,
+        pairings: ['Bloody Mary', 'Oysters', 'Eggs Benedict']
+    },
+    { 
+        id: '20', 
+        name: 'GOCHUJANG GUSTO', 
+        price: 12.49, 
+        image: require('./assets/product-placeholder.png'),
+        spicyLevel: 4,
+        pairings: ['Bibimbap', 'Korean BBQ', 'Kimchi Fried Rice']
+    },
 ];
